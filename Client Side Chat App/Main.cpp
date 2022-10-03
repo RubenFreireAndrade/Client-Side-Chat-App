9f9e27fa69ed73ef8c44f0136d8d8d3fd7fd0f78
Ruben London Met <rubenswork20@gmail.com>
2022-10-03 10:56:58 +0100

    First Commit

---
+++ Client Side Chat App/Main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<string>
+#include<SDL.h>
+#include<SDL_net.h>
+
+int main(int argc, char* argv[])
+{
+	SDL_Init(SDL_INIT_EVERYTHING);
+	std::cout << "This is Client" << std::endl;
+	return 0;
+}
